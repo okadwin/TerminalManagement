d791eeaa298d94c280cd3302ddccac184c1300ac
MY <895282428@qq.com>
2017-04-24 13:14:07 +0800

    上传验证

--- resources/views/Agent/AgentAdd.blade.php
+++ resources/views/Agent/AgentAdd.blade.php
@@ -9,27 +9,27 @@
     </div>
     <div class="content">
         <div class="formBox">
-            <form class="form-horizontal" action="{{url('AgentAdd')}}" method="post">
+            <form class="form-horizontal" action="{{url('AgentAdd')}}" method="post" id="commentForm">
                 {{ csrf_field() }}
                 <div class="col-xs-12 col-md-12 col-lg-12">
                     <!--
                     <div class="col-md-12 col-lg-6 form-group">
                         <label class="col-md-2 col-lg-4 control-label">代理商编号</label>
                         <div class="col-md-10 col-lg-8">
-                            <input type="text" class="form-control">
+                            <input type="text" class="form-control" required>
                         </div>
                     </div>
                     -->
                     <div class="col-md-12 col-lg-6 form-group">
                         <label class="col-md-2 col-lg-4 control-label">代理商名称</label>
                         <div class="col-md-10 col-lg-8">
-                            <input type="text" class="form-control" name="AgentName">
+                            <input type="text" class="form-control" name="AgentName" required>
                         </div>
                     </div>
                     <div class="col-md-12 col-lg-6 form-group">
                         <label class="col-md-2 col-lg-4 control-label">法人</label>
                         <div class="col-md-10 col-lg-8">
-                            <input type="text" class="form-control" name="Person">
+                            <input type="text" class="form-control" name="Person" required>
                         </div>
                     </div>
                 </div>
@@ -38,13 +38,13 @@
                     <div class="col-md-12 col-lg-6 form-group">
                         <label class="col-md-2 col-lg-4 control-label">联系人</label>
                         <div class="col-md-10 col-lg-8">
-                            <input type="text" class="form-control" name="cPerson">
+                            <input type="text" class="form-control" name="cPerson" required>
                         </div>
                     </div>
                     <div class="col-md-12 col-lg-6 form-group">
                         <label class="col-md-2 col-lg-4 control-label">身份证号</label>
                         <div class="col-md-10 col-lg-8">
-                            <input type="text" class="form-control" name="IDcard">
+                            <input type="text" class="form-control" name="IDcard" required>
                         </div>
                     </div>
                 </div>
@@ -53,13 +53,13 @@
                     <div class="col-md-12 col-lg-6 form-group">
                         <label class="col-md-2 col-lg-4 control-label">联系电话</label>
                         <div class="col-md-10 col-lg-8">
-                            <input type="text" class="form-control" name="cPhone">
+                            <input type="text" class="form-control" name="cPhone" required>
                         </div>
                     </div>
                     <div class="col-md-12 col-lg-6 form-group">
                         <label class="col-md-2 col-lg-4 control-label">银行账户</label>
                         <div class="col-md-10 col-lg-8">
-                            <input type="text" class="form-control" name="BankAccountNum">
+                            <input type="text" class="form-control" name="BankAccountNum" required>
                         </div>
                     </div>
                 </div>
@@ -68,26 +68,26 @@
                     <div class="col-md-12 col-lg-6 form-group">
                         <label class="col-md-2 col-lg-4 control-label">开户名</label>
                         <div class="col-md-10 col-lg-8">
-                            <input type="text" class="form-control" name="BankAccountName">
+                            <input type="text" class="form-control" name="BankAccountName" required>
                         </div>
                     </div>
                     <div class="col-md-12 col-lg-6 form-group">
                         <label class="col-md-2 col-lg-4 control-label">开户行</label>
                         <div class="col-md-10 col-lg-8">
-                            <input type="text" class="form-control" name="BankName">
+                            <input type="text" class="form-control" name="BankName" required>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-12 col-lg-6 form-group">
                     <label class="col-md-2 col-lg-4 control-label">联行号</label>
                     <div class="col-md-10 col-lg-8">
-                        <input type="text" class="form-control" name="BankNum">
+                        <input type="text" class="form-control" name="BankNum" required>
                     </div>
                 </div>
                 <div class="col-md-12 col-lg-6 form-group">
                     <label class="col-md-2 col-lg-4 control-label">分润率(%)</label>
                     <div class="col-md-10 col-lg-8">
-                        <input type="text" class="form-control" name="Profit">
+                        <input type="number" class="form-control" name="Profit" required>
                     </div>
                 </div>
                 <div class="col-xs-12 col-md-12 col-lg-12">
@@ -102,4 +102,12 @@
         </div>
     </div>
 </div>
+
+<script src="{{elixir('go/js/jquery.validate.min.js')}}"></script>
+<script src="{{elixir('go/js/jquery.messages_zh.js')}}"></script>
+<script>
+    $().ready(function() {
+        $("#commentForm").validate();
+    });
+</script>
 @endsection
